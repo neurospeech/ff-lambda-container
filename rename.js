eb288582058831699f02c96f85a3b87c51263e95
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-02-01 21:35:53 +0530

    path fixed

--- rename.js
+++ rename.js
@@ -4,4 +4,4 @@ const path = require("path");
 const ffmpeg = path.join(__dirname, "ffmpeg");
 
 const first = fs.readdirSync(ffmpeg);
-fs.renameSync(first, path.join(ffmpeg, "static"));
+fs.renameSync(first[0], path.join(ffmpeg, "static"));
