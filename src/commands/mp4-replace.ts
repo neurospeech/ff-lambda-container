f4931e57ce8ae61b82f3966ef39b277a49f6d9a0
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-02-01 23:13:52 +0530

    audio changed to aac

--- src/commands/mp4-replace.ts
+++ src/commands/mp4-replace.ts
@@ -2,7 +2,7 @@ import * as ffmpeg from "fluent-ffmpeg";
 import Command, { asJson } from "../Command";
 import TempFileService from "../TempFileService";
 
-const slow = `-preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 2500k -minrate 1500k -maxrate 4000k -bufsize 5000k -vf scale=-1:720`;
+const slow = `-preset slow -codec:a aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 2500k -minrate 1500k -maxrate 4000k -bufsize 5000k -vf scale=-1:720`;
 
 export default class Mp4Replace extends Command {
 
